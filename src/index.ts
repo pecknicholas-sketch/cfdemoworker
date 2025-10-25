e981d646822d5ce2bc245005887e5fa6f6070f68
Nicholas Peck <peck.nicholas@gmail.com>
2025-10-24 20:55:56 -0400

    agent with timestamp country username

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     	let html_content = "";
 	let timestampMS: number = Date.now();
-	html_content += "authenticated at " + timestampMS + " from " + request.cf.country;
+	const userEmail = request.headers.get("CF-Access-Authenticated-User-Email");
+	html_content += userEmail + " authenticated at " + timestampMS + " from " + request.cf.country;
     return new Response(html_content, {
       headers: {
         "content-type": "application/json;charset=UTF-8",
