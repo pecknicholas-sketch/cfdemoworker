45fd482239b8ac5ce24d3bddcf11a8a0bddeb172
pecknicholas-sketch <peck.nicholas@gmail.com>
2025-10-25 13:47:50 -0400

    Update index.ts with link

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 // index.ts
 var index_default = {
   async fetch(request, env, ctx) {
+    let html_style = `body{padding:6em; font-family: sans-serif;} h1{color:#f6821f}`;
     let html_content = "";
     let timestampMS = Date.now();
     const userEmail = request.headers.get("CF-Access-Authenticated-User-Email");
-    let country_html = "<a href=\"https://tunnel.catotonic.yachts/secure/" + request.cf.country + ">" + request.cf.country + "</a>";
+    let url = "https://tunnel.catotonic.yachts/secure/";
+    let country_html = "<a href=" + url + request.cf.country + ">" + request.cf.country + "</a>";
     html_content += userEmail + " authenticated at " + timestampMS + " from " + country_html;
-    return new Response(html_content, {
+    let html = `
+    <!DOCTYPE html>
+    <head>
+      <title>Geolocation: Weather</title>
+    </head>
+    <body>
+      <style>${html_style}</style>
+      <div id="container">
+      ${html_content}
+      </div>
+    </body>`;
+    return new Response(html, {
       headers: {
-        "content-type": "application/json;charset=UTF-8"
+        "content-type": "text/html;charset=UTF-8"
       }
     });
   }
