284dbd54b172d057b51443cbc49c600c4e5255d8
pecknicholas-sketch <peck.nicholas@gmail.com>
2025-10-25 13:37:43 -0400

    Update index.ts with test of link to country

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    	let html_content = "";
-	let timestampMS: number = Date.now();
-	const userEmail = request.headers.get("CF-Access-Authenticated-User-Email");
-	html_content += userEmail + " authenticated at " + timestampMS + " from " + request.cf.country;
+// index.ts
+var index_default = {
+  async fetch(request, env, ctx) {
+    let html_content = "";
+    let timestampMS = Date.now();
+    const userEmail = request.headers.get("CF-Access-Authenticated-User-Email");
+    let country_html = "<a href=\"https://tunnel.catotonic.yachts/secure/" + request.cf.country + ">" + request.cf.country + "</a>";
+    html_content += userEmail + " authenticated at " + timestampMS + " from " + country_html;
     return new Response(html_content, {
       headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
+        "content-type": "application/json;charset=UTF-8"
+      }
     });
-  },
-} satisfies ExportedHandler;
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
