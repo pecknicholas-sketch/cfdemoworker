4f1569ecf24559e806f94e8961e0683f745667cf
Nicholas Peck <peck.nicholas@gmail.com>
2025-10-25 20:52:27 -0400

    working V1

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 // index.ts
+interface Env {
+	COUNTRYFLAGS: cfdemocountrycode;
+}
 var index_default = {
-  async fetch(request, env, ctx) {
+  async fetch(request, env, ctx): Promise<Response> {
+    const requrl = new URL(request.url);
+    const pathSegments = requrl.pathname.split("/").filter(Boolean);
     let html_style = `body{padding:6em; font-family: sans-serif;} h1{color:#f6821f}`;
     let html_content = "";
     let timestampMS = Date.now();
     const userEmail = request.headers.get("CF-Access-Authenticated-User-Email");
-
     let countrypath = request.cf.country;
-    let url = "https://tunnel.catotonic.yachts/secure/";
-    let country_html = "<a href=" + url + request.cf.country + " id=country>" + request.cf.country + "</a>";
+    let lowcountrypath = countrypath.toLowerCase();
+    let url = "/secure/";
+    let country_html = "<a href=" + url + lowcountrypath + " id=country>" + lowcountrypath + "</a>";
     html_content += userEmail + " authenticated at " + timestampMS + " from " + country_html;
 
-    let scriptforimage = ``
     let html = `
     <!DOCTYPE html>
     <head>
@@ -22,45 +26,46 @@ var index_default = {
       <div id="container">
       ${html_content}
       </div>
-    </body>
-    <script>
-    const template = "";
+    </body>`
 
-    const bodyHistory = [];
-
-    registerHandler();
-
-    function registerHandler() {
-      document.getElementById("country").onclick = (_event) => {
-        history.pushState({}, "", "/${countrypath}");
-        const object = await this.env.countryflag.get(cfdemocountrycode/${request.cf.country} + ".svg");
-
-
-        if (object === null) {
-          return new Response("Object Not Found", { status: 404 });
-        }
-
-        bodyHistory.push(document.body.innerHTML);
-        document.body.innerHTML = template;
-      };
-    }
-
-    onpopstate = (_event) => {
-      const previousContent = bodyHistory.pop();
-
-      if (previousContent) {
-        document.body.innerHTML = previousContent;
-        registerHandler();
-      }
-    };
-  </script>`;
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8"
-      }
-    });
-  }
-};
+    if (pathSegments.length === 1 && pathSegments[0] === "secure") {
+	return new Response(html, {
+      		headers: {
+        	"content-type": "text/html;charset=UTF-8"
+      		}
+    	});
+  	}
+	if (pathSegments.length === 2 && pathSegments[0] === "secure") {
+	const lcp = pathSegments[1] + ".png";
+	try {
+	// Fetch the country flag from the private R2 bucket
+	const flagBuffer = await env.COUNTRYFLAGS.get(lcp);
+	const headers = new Headers();
+	flagBuffer.writeHttpMetadata(headers);
+	headers.set("etag", flagBuffer.httpEtag);
+	headers.set("Content-Type", "image/png");
+	console.log(headers);
+	console.log(flagBuffer.body)
+	return new Response(flagBuffer.body, {
+	headers,
+	});
+	} catch (error) {
+	console.log(error.toString());
+	// Handle errors (e.g., country flag not found)
+	return new Response("Country flag not found", {
+	status: 404,
+	headers: {
+	"Content-Type": "text/plain",
+	},
+	});
+}
+}
+	return new Response("Not a valid endpoint", {
+		status: 404,
+	headers: { "Content-Type": "text/html" },
+	});
+}
+  };
 export {
   index_default as default
 };
